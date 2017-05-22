d13bce176b912c6f945f8ef33ae5bbf67e6778f7
Emerson Knapp <emerson.b.knapp@gmail.com>
2017-05-21 22:19:27 -0700

    Start playing with some 2d platformer physics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
-#[macro_use]
-extern crate bitmask;
+mod physics;
+
+use physics::{vec2, MovingObject, AABB};
 
 use std::collections::HashSet;
 use std::path::Path;
@@ -8,19 +9,8 @@ use std::time::Duration;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Point;
-use sdl2::rect::Rect;
-
-type ID = usize;
-
-bitmask! {
-  mask ComponentMask: u32 where flags ComponentType {
-    NoComponent = 0,
-    Renderable = 1 << 0,
-    Physics = 1 << 1,
-    Input = 1 << 2,
-  }
-}
+use sdl2::rect::{Point, Rect};
+use sdl2::pixels::Color;
 
 struct Renderable {
   texture: sdl2::render::Texture,
@@ -41,84 +31,52 @@ impl Renderable {
   }
 }
 
-struct PhysicsComponent {
-    pos: Point,
+// fn draw(renderer: &mut sdl2::render::Renderer, rend: &Renderable) {
+//   let mut dest_rect = rend.source_rect;
+//   dest_rect.center_on(phys.pos);
+//   renderer.copy_ex(
+//     &rend.texture, Some(rend.source_rect), Some(dest_rect), 0.0, None, true, false)
+//     .unwrap();
+// }
+
+// fn render(renderer: &mut sdl2::render::Renderer) {
+//     renderer.clear();
+//     for e in ents {
+//       // let renderer_mask = ComponentType::Physics | ComponentType::Renderable;
+//       match (world.rendies.get(*e), world.physies.get(*e)) {
+//         (Some(&Some(ref r)), Some(&Some(ref p))) => {
+//           draw(renderer, &r, &p);
+//         },
+//         _ => {}
+//       }
+//     }
+//     renderer.present();
+// }
+
+fn aabb_to_rect(a: &AABB) -> Rect {
+  // TODO transform into render space
+  Rect::new(
+    (a.center.x - a.halfSize.x) as i32,
+    (a.center.y - a.halfSize.y) as i32,
+    (a.halfSize.x * 2.) as u32,
+    (a.halfSize.y * 2.) as u32,
+  )
 }
 
-impl PhysicsComponent {
-  fn new(pos: Point) -> PhysicsComponent {
-    PhysicsComponent {
-      pos: pos,
-    }
+fn update_player(p: &mut MovingObject, keys_down: &HashSet<Keycode>, pressed: &HashSet<Keycode>) {
+  p.speed.x = 0.;
+  if p.onGround && pressed.contains(&Keycode::Space) {
+    p.speed.y += 100.;
   }
-
-  fn update(&mut self, ticks: u32) {
-
+  if keys_down.contains(&Keycode::Left) {
+    p.speed.x -= 100.;
   }
-}
-
-struct Input {
-}
-
-struct World {
-  next: ID,
-  masks: Vec<ComponentMask>,
-  rendies: Vec<Option<Renderable>>,
-  physies: Vec<Option<PhysicsComponent>>,
-  inputs: Vec<Option<Input>>,
-}
-
-impl World {
-  fn new() -> World {
-    World {
-      next: 0,
-      masks: Vec::new(),
-      rendies: Vec::new(),
-      physies: Vec::new(),
-      inputs: Vec::new(),
-    }
+  if keys_down.contains(&Keycode::Right) {
+    p.speed.x += 100.;
   }
-}
-
-fn draw(renderer: &mut sdl2::render::Renderer, rend: &Renderable, phys: &PhysicsComponent) {
-  let mut dest_rect = rend.source_rect;
-  dest_rect.center_on(phys.pos);
-  renderer.copy_ex(
-    &rend.texture, Some(rend.source_rect), Some(dest_rect), 10.0, None, true, false)
-    .unwrap();
-}
-
-fn character(world: &mut World, renderer: &sdl2::render::Renderer) -> ID {
-  let id = world.next;
-  let rend = Renderable::new(&renderer, "assets/animate.bmp");
-  let phys = PhysicsComponent::new(Point::new(320, 240));
-  let mask = ComponentType::Renderable | ComponentType::Physics;
-  world.next += 1;
-  world.masks.push(mask);
-  world.rendies.push(Some(rend));
-  world.physies.push(Some(phys));
-  world.inputs.push(None);
-  id
-}
-
-fn render(renderer: &mut sdl2::render::Renderer, ents: &Vec<ID>, world: &World) {
-    renderer.clear();
-    for e in ents {
-      // let renderer_mask = ComponentType::Physics | ComponentType::Renderable;
-      match (world.rendies.get(*e), world.physies.get(*e)) {
-        (Some(&Some(ref r)), Some(&Some(ref p))) => {
-          draw(renderer, &r, &p);
-        },
-        _ => {}
-      }
-    }
-    renderer.present();
 }
 
 fn main() {
-  let mut world = World::new();
-
-  // Create Systems
   let sdl_context = sdl2::init().unwrap();
   let video_subsystem = sdl_context.video().unwrap();
   let window = video_subsystem.window("SDL2", 640, 480)
@@ -128,17 +86,21 @@ fn main() {
   let mut renderer = window.renderer()
     .accelerated().build().unwrap();
 
-  renderer.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
 
   let mut timer = sdl_context.timer().unwrap();
   let mut event_pump = sdl_context.event_pump().unwrap();
 
-  // Create Entities/Components
-  let mut enties : Vec<ID> = Vec::new();
-
-  enties.push(character(&mut world, &renderer));
-
   let mut prev_keys = HashSet::new();
+  let mut last_ticks = timer.ticks();
+
+  // Game Setup
+  let mut player = MovingObject {
+    pos: vec2::new(10., 10.),
+    speed: vec2::new(0., 0.),
+    bbox: AABB::new(vec2::new(10., 10.), vec2::new(20., 20.)),
+    onGround: true,
+  };
+  // End Game Setup
 
   'running: loop {
     for event in event_pump.poll_iter() {
@@ -148,35 +110,108 @@ fn main() {
       }
     }
     let ticks = timer.ticks();
+    let dt = ticks - last_ticks;
+    last_ticks = ticks;
 
     // Input
-    let keys = event_pump.keyboard_state()
+    let keys: HashSet<Keycode> = event_pump.keyboard_state()
       .pressed_scancodes()
       .filter_map(Keycode::from_scancode)
       .collect();
     let pressed = &keys - &prev_keys;
-    if pressed.contains(&Keycode::Escape) {
+    if keys.contains(&Keycode::Escape) {
       break 'running;
     }
     // let released = &prev_keys - &keys;
+
+    // Game Input
+    update_player(&mut player, &keys, &pressed);
+    // End Game Input
+
+    // Game Update
+    player.update(dt as f64 / 1000.);
+    // End Game Update
+
+    renderer.set_draw_color(Color::RGBA(0,255,0,255));
+    renderer.clear();
+    // Game Draw
+    let mut draw_color = Color::RGBA(255, 0, 0, 255);
+    if player.onGround {
+      draw_color = Color::RGBA(0, 0, 255, 255);
+    }
+    renderer.set_draw_color(draw_color);
+    let draw_rect = aabb_to_rect(&player.bbox);
+    let _ = renderer.fill_rect(draw_rect);
+    // End Game Draw
+    renderer.present();
+
+    // loop cleanup
     prev_keys = keys;
 
-    // Run Systems
-    // movement and animations
-    for e in &enties {
-      match world.physies[*e] {
-        Some(ref mut p) => p.update(ticks),
-        _ => {}
-      }
-      match world.rendies[*e] {
-        Some(ref mut r) => r.update(ticks),
-        _ => {}
-      }
-    }
-
-    // rendering
-    render(&mut renderer, &enties, &world);
-
-    std::thread::sleep(Duration::from_millis(100));
+    //std::thread::sleep(Duration::from_millis(100));
   }
 }
+
+// fn entity_main() {
+//   // Create Systems
+//   let sdl_context = sdl2::init().unwrap();
+//   let video_subsystem = sdl_context.video().unwrap();
+//   let window = video_subsystem.window("SDL2", 640, 480)
+//     .position_centered()
+//     .build()
+//     .unwrap();
+//   let mut renderer = window.renderer()
+//     .accelerated().build().unwrap();
+//
+//   renderer.set_draw_color(sdl2::pixels::Color::RGBA(0,255,0,255));
+//
+//   let mut timer = sdl_context.timer().unwrap();
+//   let mut event_pump = sdl_context.event_pump().unwrap();
+//
+//   // Create Entities/Components
+//   let mut enties : Vec<ID> = Vec::new();
+//
+//   enties.push(character(&mut world, &renderer));
+//
+//   let mut prev_keys = HashSet::new();
+//
+//   'running: loop {
+//     for event in event_pump.poll_iter() {
+//       match event {
+//         Event::Quit {..} => break 'running,
+//         _ => {}
+//       }
+//     }
+//     let ticks = timer.ticks();
+//
+//     // Input
+//     let keys = event_pump.keyboard_state()
+//       .pressed_scancodes()
+//       .filter_map(Keycode::from_scancode)
+//       .collect();
+//     let pressed = &keys - &prev_keys;
+//     if pressed.contains(&Keycode::Escape) {
+//       break 'running;
+//     }
+//     // let released = &prev_keys - &keys;
+//     prev_keys = keys;
+//
+//     // Run Systems
+//     // movement and animations
+//     for e in &enties {
+//       match world.physies[*e] {
+//         Some(ref mut p) => p.update(ticks),
+//         _ => {}
+//       }
+//       match world.rendies[*e] {
+//         Some(ref mut r) => r.update(ticks),
+//         _ => {}
+//       }
+//     }
+//
+//     // rendering
+//     render(&mut renderer, &enties, &world);
+//
+//     std::thread::sleep(Duration::from_millis(100));
+//   }
+// }
---
+++ src/physics.rs
@@ -0,0 +1,68 @@
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+extern crate nalgebra as na;
+
+pub type vec2 = self::na::Vector2<f64>;
+
+pub struct AABB {
+  pub center: vec2,
+  pub halfSize: vec2,
+}
+
+impl AABB {
+  pub fn new(center: vec2, halfSize: vec2) -> AABB {
+    AABB {
+      center: center,
+      halfSize: halfSize,
+    }
+  }
+  pub fn intersects(&self, other: &AABB) -> bool {
+    ! (
+      ((self.center.x - other.center.x).abs() > (self.halfSize.x + other.halfSize.x)) ||
+      ((self.center.y - other.center.y).abs() > self.halfSize.y + other.halfSize.y)
+    )
+  }
+}
+
+pub struct MovingObject {
+  pub pos: vec2,
+  pub speed: vec2,
+  pub bbox: AABB,
+  pub onGround: bool,
+}
+
+impl MovingObject {
+  pub fn update(&mut self, dt_seconds: f64) {
+    self.pos += self.speed * dt_seconds;
+    self.speed.y -= 200. * dt_seconds;
+
+    if self.pos.y <= self.bbox.halfSize.y {
+      self.pos.y = self.bbox.halfSize.y;
+      self.speed.y = 0.;
+      self.onGround = true;
+    } else {
+      self.onGround = false;
+    }
+
+    self.bbox.center = self.pos;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn ctor() {
+    let a = AABB::new(vec2::new(0., 0.), vec2::new(1., 1.));
+  }
+  #[test]
+  fn intersections() {
+    let a = AABB::new(vec2::new(0., 0.), vec2::new(1., 1.));
+    let b = AABB::new(vec2::new(5., 5.), vec2::new(1., 1.));
+    assert!(!a.intersects(&b));
+
+    let c = AABB::new(vec2::new(1., 1.), vec2::new(1., 1.));
+    assert!(a.intersects(&c));
+  }
+}
