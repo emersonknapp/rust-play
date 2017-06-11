d83dc8027d53078b69aede62c70a1f72799338b9
Emerson Knapp <emerson.b.knapp@gmail.com>
2017-06-11 16:53:49 -0700

    Phys restructure

--- src/physics.rs
+++ src/physics.rs
@@ -1,5 +1,5 @@
 use std::time;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 use common::{Vec2, Vec2u};
 use components::{Velocity, Position, World, Collision};
@@ -11,13 +11,26 @@ fn camera_follow(camera_pos: &mut Position, following_pos: &Position) {
   camera_pos.x = following_pos.x;
 }
 
-fn physics_update(velocity: &mut Velocity, position: &mut Position,
-    collision: &Collision, tilemap: &Tilemap, on_ground: &mut bool, dt_seconds: f64)
-{
+fn movement_update(position: &Position, velocity: &Velocity, dt_seconds: f64) -> (Position, Velocity) {
   let dpos = *velocity * dt_seconds;
   let next = *position + dpos;
-  velocity.y -= GRAVITY * dt_seconds;
+  let next_vel = velocity - Vec2::new(0., GRAVITY * dt_seconds);
+  (next, next_vel)
+}
 
+pub fn physics_step(w: &mut World, dt_seconds: f64, statics_collisions: &mut HashSet<usize>) {
+  // move everything first
+  // TODO: don't resolve tilemap collisions here, do it in collision detection & resolution phase
+  let mut updates: Vec<(usize, Position, Velocity, bool)> = Vec::new();
+
+  for id in &w.entities {
+    if let Some((pos, _, vel)) = w.get_dynamic_entity(*id) {
+      let (next_pos, next_vel) = movement_update(pos, vel, dt_seconds);
+      updates.push((*id, next_pos, next_vel, false));
+    }
+  }
+
+  /* tilemap collision
   let mut test = Vec2::new(next.x, position.y);
   // TODO collide with multiple tilemaps?
   let mut test_intersections: Vec<Vec2u> = tilemap.intersects_box(&collision.offset(test));
@@ -37,8 +50,39 @@ fn physics_update(velocity: &mut Velocity, position: &mut Position,
       velocity.y = velocity.y.min(0.);
     }
   }
+  */
 
-  *position = test;
+  // detect statics collisions
+  for id in &w.entities {
+    if let Some((spos, scol)) = w.get_static_entity(*id) {
+      for &(uid, ppos, _, _) in &updates {
+        if let Some(pcol) = w.collisions.get(&uid) {
+          if pcol.offset(ppos).intersects(&scol.offset(*spos)) {
+            statics_collisions.insert(*id);
+          }
+        }
+      }
+    }
+  }
+
+  // resolve collisions
+  // don't allow fall below 0 (for testing)
+  for idx in 0..updates.len() {
+    if updates[idx].1.y <= 0. {
+      updates[idx].1.y = 0.;
+      updates[idx].2.y = 0.;
+      updates[idx].3 = true;
+    }
+  }
+
+  // finalize
+  for (id, pos, vel, ground) in updates {
+    w.positions.insert(id, pos);
+    w.velocities.insert(id, vel);
+    if let Some(_) = w.groundables.get(&id) {
+      w.groundables.insert(id, ground);
+    }
+  }
 }
 
 pub fn simulation_systems(w: &mut World, dt: time::Duration) -> time::Duration {
@@ -49,48 +93,21 @@ pub fn simulation_systems(w: &mut World, dt: time::Duration) -> time::Duration {
 
   let mut statics_collisions: HashSet<usize> = HashSet::new();
 
-  //PROPOSAL
-  //1. physically update everything (new Position & Velocity, need to keep old)
-  //2. detect collisions
-  //3. back out collisions (which will affect Velocity based on the original going in)
-  for id in &w.entities {
-    if let Some((ppos, pcol, _)) = w.get_dynamic_entity(*id) {
-      for id in &w.entities {
-        if let Some((spos, scol)) = w.get_static_entity(*id) {
-          if pcol.offset(*ppos).intersects(&scol.offset(*spos)) {
-            statics_collisions.insert(*id);
-          }
-        }
-      }
-    }
-  }
-
-  w.statics_collisions = statics_collisions;
-  //TODO now that i was capable of writing this loop, how to resolve collisions?
-
 
   while dt_accum >= sim_dt {
-    for id in &w.entities {
-      // run physics
-      if let (
-        Some(ref mut velocity), Some(ref mut position), Some(ref collision), Some(ref mut on_ground), Some(ref tilemap)
-      ) = (
-        w.velocities.get_mut(&id), w.positions.get_mut(&id), w.collisions.get(&id), w.groundables.get_mut(&id), w.tilemaps.get(&w.current_tilemap)
-      ) {
-        physics_update(velocity, position, collision, tilemap, on_ground, sim_dt_secs);
-      }
-
-      // update camera follow
-      if let (Some(ref mut camera), Some(ref player_pos)) =
-             (w.cameras.get_mut(&w.current_camera), w.positions.get(&w.current_player))
-      {
-        camera_follow(&mut camera.pos, player_pos)
-      }
-      // TODO reactions to physics collisions?
-
-    }
+    physics_step(w, sim_dt_secs, &mut statics_collisions);
     dt_accum -= sim_dt;
   }
+
+  // update camera follow
+  if let (Some(ref mut camera), Some(ref player_pos)) =
+         (w.cameras.get_mut(&w.current_camera), w.positions.get(&w.current_player))
+  {
+    camera_follow(&mut camera.pos, player_pos)
+  }
+  // TODO reactions (damage, animation, sound) to physics collisions?
+
+  w.statics_collisions = statics_collisions;
 
   dt_accum
 }
